4cc5ab3dae533b1520d61c84a599148a6019b2e3
Jason Brooks <JasonBrooksBio@gmail.com>
2019-05-09 11:15:01 -0500

    Added button listeners in main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,29 @@
-import sayHello from "./hello"
-import sayGoodbye from "./goodbye"
-import SandwichMaker from "./sandwichMaker"
 
-sayHello()
-sayGoodbye()
 
-SandwichMaker.placeOrder("rye", "capicola", "provolone")
+const sign_in_btn = document.querySelector("#sign-in-btn")
+const sign_up_btn = document.querySelector("#sign-up-btn")
+
+
+sign_in_btn.addEventListener("click", function (e) {
+
+    let username = document.querySelector("#sign-in-username").value
+    let password = document.querySelector("#sign-in-password").value
+
+    console.log(username + " " + password)
+    //Add code here to ADD user
+
+    //Then call function to update DOM
+    //At this point SIGN UP SUCCESSFUL! or USERNAME NOT FOUND - SIGN UP!
+})
+sign_up_btn.addEventListener("click", function (e) {
+
+    let username = document.querySelector("#sign-up-username").value
+    let password = document.querySelector("#sign-up-password").value
+
+    console.log(username + " " + password)
+    //Add code here to ADD user
+
+    //Then call function to update DOM
+    //At this point SIGN UP SUCCESSFUL! or USER NAME ALREADY IN USE!
+})
+
