52394068d58497b46da156f43556e0bf38c54d16
Ken Dixon <KDixon@Kens-Macbook-Air.local>
2019-05-09 13:33:28 -0500

    changes to README, aliases.yam, and bgShell

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 import API from "./dataFetch"
 import eventsForm from "./events";
 // API.delete(2)
-eventsForm.createEventForm()
+// eventsForm.createEventForm()
 
 const sign_in_btn = document.querySelector("#sign-in-btn")
 const sign_up_btn = document.querySelector("#sign-up-btn")
