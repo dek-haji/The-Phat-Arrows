09303aa31b6c065c9f9da4a75056e7e057b6bfcd
Jason Brooks <JasonBrooksBio@gmail.com>
2019-05-09 15:06:40 -0500

    Added user log in authentication and sessionstorage stuff

--- src/scripts/dataFetch.js
+++ src/scripts/dataFetch.js
@@ -1,4 +1,4 @@
-                // this will be our fetch calls component.
+// this will be our fetch calls component.
 
 
 const taskUrl = "http://localhost:3000/tasks";
@@ -9,59 +9,67 @@ const eventsUrl = "http://localhost:3000/events"
 
 
 const API = {
-    getAll: function() {
-      return fetch(eventsUrl)
+
+  userLogin: function(username, password) {
+    return fetch(`http://localhost:3000/users?user_name=${username}&password=${password}`)
       .then(results => results.json())
       .then(parsedEvents => {
-        console.log("this is get all",parsedEvents)
+        return parsedEvents
       })
     },
-    getOne: function(Id) {
-      return fetch(`${eventsUrl}/${Id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }).then(response => response.json())
-      .then(parsedSingle => {
-          console.log("this is a single obj",parsedSingle)
-      });
+  getAll: function() {
+    return fetch(eventsUrl)
+      .then(results => results.json())
+      .then(parsedEvents => {
+        console.log("this is get all", parsedEvents)
+      })
     },
-    save: function(eventObject) {
-      return fetch(baseUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(eventObject)
-      }).then(response => response.json())
-      .then(savedData => {
-          console.log("this is savedData",savedData)
-      });
-    },
-    delete: function(eventId) {
-      return fetch(`${eventsUrl}/${eventId}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }).then(response => response.json())
-      .then(console.log("succesfully deleted"));
-    },
-    edit: function(id, object) {
-      return fetch(`${baseUrl}/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(object)
-      }).then(response => response.json())
-      .then(editedDated => {
-          console.log("this is edited stuff", editedDated)
-      });
+getOne: function(Id) {
+  return fetch(`${eventsUrl}/${Id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
     }
+  }).then(response => response.json())
+    .then(parsedSingle => {
+      console.log("this is a single obj", parsedSingle)
+    });
+},
+save: function(eventObject) {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(eventObject)
+  }).then(response => response.json())
+    .then(savedData => {
+      console.log("this is savedData", savedData)
+    });
+},
+delete: function(eventId) {
+  return fetch(`${eventsUrl}/${eventId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(response => response.json())
+    .then(console.log("succesfully deleted"));
+},
+edit: function(id, object) {
+  return fetch(`${baseUrl}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(object)
+  }).then(response => response.json())
+    .then(editedDated => {
+      console.log("this is edited stuff", editedDated)
+    });
+}
 
   }
 
 
-  export default API
+export default API
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,38 @@
 import API from "./dataFetch"
 import eventsForm from "./events";
 // API.delete(2)
-eventsForm.createEventForm()
+//eventsForm.createEventForm()
 
 const sign_in_btn = document.querySelector("#sign-in-btn")
 const sign_up_btn = document.querySelector("#sign-up-btn")
+
 
 
 sign_in_btn.addEventListener("click", function (e) {
 
     let username = document.querySelector("#sign-in-username").value
     let password = document.querySelector("#sign-in-password").value
+    //Check to make sure fields aren't blank
+    if (username === "" || password === "") {
+        window.alert("Please Enter Username & Password")
+    }
+    API.userLogin(username, password)
+        .then(result => {
+            if (result.length === 0){
+                sessionStorage.clear();
+                window.alert("Username and/or Password not found!")
+            }
+            else {
+               // let data = String(result[0].id)
+                let data = result[0].id
+                //console.log(`User ID is equal to ${data}`)
+                sessionStorage.setItem("session_user_id", data)
+                //console.log("HELLO")
 
-    console.log(username + " " + password)
+            }
+        })
+
+    //console.log(username + " " + password)
     //Add code here to ADD user
 
     //Then call function to update DOM
@@ -23,6 +43,11 @@ sign_up_btn.addEventListener("click", function (e) {
 
     let username = document.querySelector("#sign-up-username").value
     let password = document.querySelector("#sign-up-password").value
+    //Check to make sure fields aren't blank
+    if (username === "" || password === "") {
+        window.alert("Please Enter Username & Password")
+    }
+
 
     console.log(username + " " + password)
     //Add code here to ADD user
